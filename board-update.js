c03e684d671663ce48952cb849b3c0881a108a69
Jahid <hzahid905@gmail.com>
2025-03-02 06:15:06 +0600

    add blog page

--- board-update.js
+++ board-update.js
@@ -19,9 +19,8 @@ document.getElementById("card-btn-1").addEventListener("click", function () {
     "p-4"
   );
   let cardTitle = document.getElementById("card-title-1").innerText;
-  let currentTime = new Date().toLocaleTimeString();
   historyAdd.innerText =
-    "You have Complete The Task " + cardTitle + " at " + currentTime;
+    "You have Complete The Task " + cardTitle + " at " + localTime;
   let historyContainer = document.getElementById("history-container");
   historyContainer.appendChild(historyAdd);
 });
@@ -47,9 +46,9 @@ document.getElementById("card-btn-2").addEventListener("click", function () {
     "p-4"
   );
   let cardTitle2 = document.getElementById("card-title-2").innerText;
-  let currentTime = new Date().toLocaleTimeString();
+
   historyAdd.innerText =
-    "You have Complete The Task " + cardTitle2 + " at " + currentTime;
+    "You have Complete The Task " + cardTitle2 + " at " + localTime;
   let historyContainer = document.getElementById("history-container");
   historyContainer.appendChild(historyAdd);
 });
@@ -75,9 +74,8 @@ document.getElementById("card-btn-3").addEventListener("click", function () {
     "p-4"
   );
   let cardTitle3 = document.getElementById("card-title-3").innerText;
-  let currentTime = new Date().toLocaleTimeString();
   historyAdd.innerText =
-    "You have Complete The Task " + cardTitle3 + " at " + currentTime;
+    "You have Complete The Task " + cardTitle3 + " at " + localTime;
   let historyContainer = document.getElementById("history-container");
   historyContainer.appendChild(historyAdd);
 });
@@ -103,9 +101,8 @@ document.getElementById("card-btn-4").addEventListener("click", function () {
     "p-4"
   );
   let cardTitle4 = document.getElementById("card-title-4").innerText;
-  let currentTime = new Date().toLocaleTimeString();
   historyAdd.innerText =
-    "You have Complete The Task " + cardTitle4 + " at " + currentTime;
+    "You have Complete The Task " + cardTitle4 + " at " + localTime;
   let historyContainer = document.getElementById("history-container");
   historyContainer.appendChild(historyAdd);
 });
@@ -131,9 +128,8 @@ document.getElementById("card-btn-5").addEventListener("click", function () {
     "p-4"
   );
   let cardTitle5 = document.getElementById("card-title-5").innerText;
-  let currentTime = new Date().toLocaleTimeString();
   historyAdd.innerText =
-    "You have Complete The Task " + cardTitle5 + " at " + currentTime;
+    "You have Complete The Task " + cardTitle5 + " at " + localTime;
   let historyContainer = document.getElementById("history-container");
   historyContainer.appendChild(historyAdd);
 });
@@ -159,9 +155,8 @@ document.getElementById("card-btn-6").addEventListener("click", function () {
     "p-4"
   );
   let cardTitle6 = document.getElementById("card-title-6").innerText;
-  let currentTime = new Date().toLocaleTimeString();
   historyAdd.innerText =
-    "You have Complete The Task " + cardTitle6 + " at " + currentTime;
+    "You have Complete The Task " + cardTitle6 + " at " + localTime;
   let historyContainer = document.getElementById("history-container");
   historyContainer.appendChild(historyAdd);
 });
---
+++ localTime.js
@@ -0,0 +1,38 @@
+let localTime = new Date().toLocaleTimeString();
+
+let currentDate = new Date();
+
+let day = currentDate.getDay();
+let month = currentDate.getMonth();
+let date = currentDate.getDate();
+let year = currentDate.getFullYear();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+document.getElementById("day").innerText = days[day].slice(0, 3);
+document.getElementById("month").innerText = months[month];
+document.getElementById("date").innerText = date;
+document.getElementById("year").innerText = year;
